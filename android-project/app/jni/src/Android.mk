0ba33d210e085120279d2a99f17818d3c0ab2f1b
Andy Vandijck <andyvand@gmail.com>
2025-02-10 12:49:47 +0100

    Fix MIDI playback (fluidsynth) android
    Fix MIDI playback (fluidsynth) android

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -30,6 +30,13 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := fluidsynth
+LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/$(APP_ABI)/libfluidsynth.so
+#LOCAL_EXPORT_C_INCLUDES :=
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE := midiproc
 LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/$(APP_ABI)/libmidiproc_shared.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/midiproc
@@ -45,6 +52,6 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 LOCAL_SRC_FILES := ../../../../src/animlib.c ../../../../src/arg_parse.c ../../../../src/backgrnd.c ../../../../src/config.c ../../../../src/config_file.c ../../../../src/destruct.c ../../../../src/editship.c ../../../../src/episodes.c ../../../../src/file.c ../../../../src/font.c ../../../../src/fonthand.c ../../../../src/game_menu.c ../../../../src/helptext.c ../../../../src/joystick.c ../../../../src/jukebox.c ../../../../src/keyboard.c ../../../../src/lds_play.c ../../../../src/loudness.c ../../../../src/lvllib.c ../../../../src/lvlmast.c ../../../../src/mainint.c ../../../../src/menus.c ../../../../src/mouse.c ../../../../src/mtrand.c ../../../../src/musmast.c ../../../../src/network.c ../../../../src/nortsong.c ../../../../src/nortvars.c ../../../../src/opentyr.c ../../../../src/opl.c ../../../../src/palette.c ../../../../src/params.c ../../../../src/pcxload.c ../../../../src/pcxmast.c ../../../../src/picload.c ../../../../src/player.c ../../../../src/shots.c ../../../../src/sizebuf.c ../../../../src/sndmast.c ../../../../src/sprite.c ../../../../src/starlib.c ../../../../src/tyrian2.c ../../../../src/varz.c ../../../../src/vga256d.c ../../../../src/vga_palette.c ../../../../src/video.c ../../../../src/video_scale.c ../../../../src/video_scale_hqNx.c ../../../../src/xmas.c
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid -lc++ -lc++abi
-LOCAL_SHARED_LIBRARIES := SDL3 SDL3_net SDL3_mixer midiproc c++_shared
+LOCAL_SHARED_LIBRARIES := SDL3 SDL3_net SDL3_mixer fluidsynth midiproc c++_shared
 
 include $(BUILD_SHARED_LIBRARY)
