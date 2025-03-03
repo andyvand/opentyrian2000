dde223a55ecf677f0eceec108bb6fbf742207415
Andy Vandijck <andyvand@gmail.com>
2025-03-03 12:19:52 +0100

    Fix mistake
    Fix mistake

--- components/SDL/SDL_event.c
+++ components/SDL/SDL_event.c
@@ -204,7 +204,7 @@ int SDL_PollEvent(SDL_Event * event)
 
     event->motion.state = esp_lcd_touch_get_coordinates(tp, x, y, strength, &count, 1) ? SDL_PRESSED : SDL_RELEASED;
     event->motion.type = event->motion.state == SDL_PRESSED ? SDL_MOUSEBUTTONDOWN : SDL_MOUSEBUTTONUP;
-    event->type = event->motion.state === SDL_PRESSED ? SDL_MOUSEBUTTONDOWN : SDL_MOUSEBUTTONUP;
+    event->type = event->motion.state == SDL_PRESSED ? SDL_MOUSEBUTTONDOWN : SDL_MOUSEBUTTONUP;
     event->motion.x = x[0];
     event->motion.y = y[0];
 
